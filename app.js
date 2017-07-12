fe88e79a4911f7b34f42eba3c4893ecb342b2024
ghigbie <gehigbie@gmail.com>
2017-07-12 16:06:53 +0000

    Add / route

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require('express'),
 const port = process.env.PORT,
         ip = process.env.IP;
 
+app.get('/', (req, res) =>{
+    console.log('On the / page');
+    res.send("Yo");
+});
+
 app.listen(port, ip, ()=> {
    console.log(`Server is listening on ${port}`); 
 });
