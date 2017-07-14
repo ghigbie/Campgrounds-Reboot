a6cfab27f6e32e7efb51026b4e6796886a873ce6
ghigbie <gehigbie@gmail.com>
2017-07-14 13:52:31 +0000

    Create not found page

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const express = require('express'),
 const port = process.env.PORT,
         ip = process.env.IP;
 
-var campgrounds = [
-    {name: "Salman Hunt", image: "http://www.switchbacktravel.com/sites/default/files/images/articles/Colorado%20Outdoors.jpg"},
-    {name: "Hunter's Cove", image: "http://wilderness.org/sites/default/files/styles/blog_full/public/boots%20at%20lake%20photo.jpg?itok=nIFuJ6G5"},
-    {name: "Resting Trees", image: "http://www.heartsandmindsbooks.com/Hiking-Shoes-588x391.jpg"},
-    {name: "Lookout Range", image: "http://visitsedona.com/wp-content/uploads/2015/05/peach-sky-web.jpg"},
-    ];
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
@@ -20,8 +13,17 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/campgrounds', (req, res) => {
+    var campgrounds = [
+        {name: "Salmon Hunt", image: "http://www.switchbacktravel.com/sites/default/files/images/articles/Colorado%20Outdoors.jpg"},
+        {name: "Hunter's Cove", image: "http://wilderness.org/sites/default/files/styles/blog_full/public/boots%20at%20lake%20photo.jpg?itok=nIFuJ6G5"},
+        {name: "Resting Trees", image: "http://www.heartsandmindsbooks.com/Hiking-Shoes-588x391.jpg"},
+        {name: "Lookout Range", image: "http://visitsedona.com/wp-content/uploads/2015/05/peach-sky-web.jpg"}, ];
     console.log('On the /campgrounds page');
     res.send("Yo campgrounds");
+});
+
+app.get('*', (req, res) => {
+    res.render('notfound');
 });
 
 app.listen(port, ip, ()=> {
